f9efaf892d78183a47fa6d0bfe82986cd5bab3ce
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2026-08-07 21:21:14 -0400

    Initial Commit

---
+++ bulletproof/bulletproofApp.swift
@@ -0,0 +1,17 @@
+//
+//  bulletproofApp.swift
+//  bulletproof
+//
+//  Created by Mahdi Yusuf on 2026-08-07.
+//
+
+import SwiftUI
+
+@main
+struct bulletproofApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bulletproofTests/bulletproofTests.swift
@@ -0,0 +1,19 @@
+//
+//  bulletproofTests.swift
+//  bulletproofTests
+//
+//  Created by Mahdi Yusuf on 2026-08-07.
+//
+
+import Testing
+@testable import bulletproof
+
+struct bulletproofTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
